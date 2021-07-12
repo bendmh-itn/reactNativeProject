ffb5b278dce0436f3cefda96462e49e0050eb6b5
ben dmh <demab@site.asty-moulin.be>
2021-07-12 18:09:37 +0200

    my first commit

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  Button,
+  Alert,
+  StatusBar,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("text pressed");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Text>Test n°2</Text>
+      {/*<TouchableWithoutFeedback onPress={() => console.log("image tapped")}>
+        <Image
+          blurRadius={10}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300"
+          }} 
+        />
+        </TouchableWithoutFeedback>*/}
+
+      <Button
+        title="Click me"
+        color="#841584"
+        onPress={() =>
+          Alert.alert("My title", "My message", [
+            { text: "Yes", onPress: () => console.log("yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      />
+    </SafeAreaView>
   );
 }
+
+const containerStyle = { backgroundColor: "orange" };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: (Platform.OS = "android" ? StatusBar.currentHeight : 0),
   },
 });
