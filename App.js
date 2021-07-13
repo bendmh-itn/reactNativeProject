7da9b4fcc86c1a040e07b05542dfe6381216fced
ben dmh <demab@site.asty-moulin.be>
2021-07-13 16:24:59 +0200

    ajout de plusieurs écrans

--- App.js
+++ App.js
@@ -11,46 +11,20 @@ import {
   StatusBar,
   Platform,
 } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  const handlePress = () => {
-    console.log("text pressed");
-  };
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Text>Test n°2</Text>
-      {/*<TouchableWithoutFeedback onPress={() => console.log("image tapped")}>
-        <Image
-          blurRadius={10}
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300"
-          }} 
-        />
-        </TouchableWithoutFeedback>*/}
-
-      <Button
-        title="Click me"
-        color="#841584"
-        onPress={() =>
-          Alert.alert("My title", "My message", [
-            { text: "Yes", onPress: () => console.log("yes") },
-            { text: "No", onPress: () => console.log("No") },
-          ])
-        }
-      />
+    <SafeAreaView style={styles.container}>
+      <WelcomeScreen />
     </SafeAreaView>
   );
 }
 
-const containerStyle = { backgroundColor: "orange" };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: (Platform.OS = "android" ? StatusBar.currentHeight : 0),
+    paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 0,
   },
 });
